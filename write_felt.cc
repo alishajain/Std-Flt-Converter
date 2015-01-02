c22d2d43e4297dc6ab6762750e172b8c3db1a8d0
alishajain <jain.alisha14@gmail.com>
2015-01-02 23:51:46 +0530

    Converts the basic std file to flt file

---
+++ write_felt.cc
@@ -0,0 +1,75 @@
+#include "write_felt.h"
+
+write::write() {
+	jc_count = 0;
+	mi_count = 0;
+}
+
+void write::store_values(char nodes_beams, float value, int i) {
+	if (nodes_beams == 'n') {
+	    jc[i] = value;
+	    jc_count++;
+	} else if (nodes_beams == 'b') {
+	    mi[i] = value;
+	    mi_count++;
+	}
+}
+
+void write::write_description(string f_name) {
+	out_file = f_name;
+	ofstream f(out_file.c_str(), ios::out);
+	f << "problem description" << endl;
+}
+
+void write::write_title() {
+	ofstream f(out_file.c_str(), ios::app);
+	f << "title=\"Beam sample problem\"" << endl;
+}
+
+void write::total_node_beam() {
+	ofstream f(out_file.c_str(), ios::app);
+
+	int nodes, elements;
+	nodes = jc_count/3;
+	elements = mi_count/2;
+	f << "nodes=" << nodes << " elements=" << elements << endl;
+}
+
+void write::write_nodes() {
+	total_node_beam(); 
+	ofstream f(out_file.c_str(), ios::app);
+	f << endl << "nodes" << endl;
+
+	for (int j = 0, i = 1; j < jc_count; j++) {
+cout << " jc " << jc[j] << endl;
+	    if (j % 3 == 0) {
+		f << i << "  x=" << jc[j];
+		i++;
+	    } else if (j % 3 == 1) {
+		f << " y=" << jc[j];
+	    } else if (j % 3 == 2) {
+		f << " z=" << jc[j] << endl;
+	    }
+	}
+}
+
+void write::write_beams() {
+	ofstream f(out_file.c_str(), ios::app);
+	f << endl << "beam elements" << endl;
+
+        for (int j = 0, i = 1; j < mi_count; j++) {
+cout << " mi " << mi[j] << endl;
+	    if (j % 2 == 0) {
+		f << i << "  nodes=[" << mi[j];
+		i++;
+	    } else if (j % 2 == 1) {
+		f << "," << mi[j] << "]" << endl;
+	    }
+	}
+}
+
+void write::write_end_file() {
+	ofstream f(out_file.c_str(), ios::app);
+	f << endl << "end";
+}
+
